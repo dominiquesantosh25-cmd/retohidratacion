32e4cac5e0a439a7e877c4569a05693efa705425
domi <dominiquesantosh25@cetis146.edu.mx>
2026-04-23 08:14:35 -0600

    Fix color references in register_view.py

--- views/register_view.py
+++ views/register_view.py
@@ -9,28 +9,28 @@ class RegisterView:
         titulo = ft.Text(
             value="Crear Cuenta", 
             size=30, 
-            color=ft.colors.BLUE_900, 
+            color=ft.Colors.BLUE_900, 
             weight=ft.FontWeight.BOLD
         )
         
         subtitulo = ft.Text(
             value="¡Únete al reto de la hidratación!",
             size=16,
-            color=ft.colors.BLUE_GREY_700
+            color=ft.Colors.BLUE_GREY_700
         )
 
         # 2. Campos de entrada de datos
         input_nombre = ft.TextField(
             label="Nombre Completo",
             prefix_icon=ft.icons.PERSON_OUTLINE,
-            border_color=ft.colors.BLUE_400,
+            border_color=ft.Colors.BLUE_400,
             hint_text="Ej. Ana Rodríguez"
         )
 
         input_correo = ft.TextField(
             label="Correo Electrónico",
             prefix_icon=ft.icons.EMAIL_OUTLINED,
-            border_color=ft.colors.BLUE_400,
+            border_color=ft.Colors.BLUE_400,
             keyboard_type=ft.KeyboardType.EMAIL,
             hint_text="ejemplo@correo.com"
         )
@@ -38,23 +38,23 @@ class RegisterView:
         input_edad = ft.TextField(
             label="Edad",
             prefix_icon=ft.icons.CAKE_OUTLINED,
-            border_color=ft.colors.BLUE_400,
+            border_color=ft.Colors.BLUE_400,
             keyboard_type=ft.KeyboardType.NUMBER,
             width=150 # La edad no necesita tanto espacio
         )
 
         # 3. Botones
         boton_registrar = ft.ElevatedButton(
-            text="Finalizar Registro",
-            bgcolor=ft.colors.CYAN_700,
-            color=ft.colors.WHITE,
+            "Finalizar Registro",
+            bgcolor=ft.Colors.CYAN_700,
+            color=ft.Colors.WHITE,
             width=250,
             height=50,
             on_click=lambda _: print("Datos capturados: ", input_nombre.value) # Luego conectaremos al controlador
         )
 
         boton_regresar = ft.TextButton(
-            text="¿Ya tienes cuenta? Inicia sesión",
+            "¿Ya tienes cuenta? Inicia sesión",
             on_click=lambda _: self.page.go("/login")
         )
 
